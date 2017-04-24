df2c326931f425bc8bc6dcbdb5771854ddc879c1
Arvind Pandey <arpandey@amazon.com>
2017-04-24 15:36:17 +0530

    changes to train a small neural net

--- Assignment1.py
+++ Assignment1.py
@@ -15,78 +15,78 @@ from six.moves import cPickle as pickle
 
 ### For downloading the MNIST like data
 
-# url = 'http://commondatastorage.googleapis.com/books1000/'
-# last_percent_reported = None
-# data_root = '.' # Change me to store data elsewhere
+url = 'http://commondatastorage.googleapis.com/books1000/'
+last_percent_reported = None
+data_root = '.' # Change me to store data elsewhere
 
-# def download_progress_hook(count, blockSize, totalSize):
-#   """A hook to report the progress of a download. This is mostly intended for users with
-#   slow internet connections. Reports every 5% change in download progress.
-#   """
-#   global last_percent_reported
-#   percent = int(count * blockSize * 100 / totalSize)
+def download_progress_hook(count, blockSize, totalSize):
+  """A hook to report the progress of a download. This is mostly intended for users with
+  slow internet connections. Reports every 5% change in download progress.
+  """
+  global last_percent_reported
+  percent = int(count * blockSize * 100 / totalSize)
 
-#   if last_percent_reported != percent:
-#     if percent % 5 == 0:
-#       sys.stdout.write("%s%%" % percent)
-#       sys.stdout.flush()
-#     else:
-#       sys.stdout.write(".")
-#       sys.stdout.flush()
+  if last_percent_reported != percent:
+    if percent % 5 == 0:
+      sys.stdout.write("%s%%" % percent)
+      sys.stdout.flush()
+    else:
+      sys.stdout.write(".")
+      sys.stdout.flush()
       
-#     last_percent_reported = percent
+    last_percent_reported = percent
         
-# def maybe_download(filename, expected_bytes, force=False):
-#   """Download a file if not present, and make sure it's the right size."""
-#   dest_filename = os.path.join(data_root, filename)
-#   if force or not os.path.exists(dest_filename):
-#     print('Attempting to download:', filename) 
-#     filename, _ = urlretrieve(url + filename, dest_filename, reporthook=download_progress_hook)
-#     print('\nDownload Complete!')
-#   statinfo = os.stat(dest_filename)
-#   if statinfo.st_size == expected_bytes:
-#     print('Found and verified', dest_filename)
-#   else:
-#     raise Exception(
-#       'Failed to verify ' + dest_filename + '. Can you get to it with a browser?')
-#   return dest_filename
+def maybe_download(filename, expected_bytes, force=False):
+  """Download a file if not present, and make sure it's the right size."""
+  dest_filename = os.path.join(data_root, filename)
+  if force or not os.path.exists(dest_filename):
+    print('Attempting to download:', filename) 
+    filename, _ = urlretrieve(url + filename, dest_filename, reporthook=download_progress_hook)
+    print('\nDownload Complete!')
+  statinfo = os.stat(dest_filename)
+  if statinfo.st_size == expected_bytes:
+    print('Found and verified', dest_filename)
+  else:
+    raise Exception(
+      'Failed to verify ' + dest_filename + '. Can you get to it with a browser?')
+  return dest_filename
 
-# train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
-# test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
+train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
 
 
 #### For downloading MNIST like data finishes here############
 
 ##################extracting the data##############################
-# num_classes = 10
-# np.random.seed(133)
+num_classes = 10
+np.random.seed(133)
 
-# def maybe_extract(filename, force=False):
-#   root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
-#   if os.path.isdir(root) and not force:
-#     # You may override by setting force=True.
-#     print('%s already present - Skipping extraction of %s.' % (root, filename))
-#   else:
-#     print('Extracting data for %s. This may take a while. Please wait.' % root)
-#     tar = tarfile.open(filename)
-#     sys.stdout.flush()
-#     tar.extractall(data_root)
-#     tar.close()
-#   data_folders = [
-#     os.path.join(root, d) for d in sorted(os.listdir(root))
-#     if os.path.isdir(os.path.join(root, d))]
-#   if len(data_folders) != num_classes:
-#     raise Exception(
-#       'Expected %d folders, one per class. Found %d instead.' % (
-#         num_classes, len(data_folders)))
-#   print(data_folders)
-#   return data_folders
+def maybe_extract(filename, force=False):
+  root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+  if os.path.isdir(root) and not force:
+    # You may override by setting force=True.
+    print('%s already present - Skipping extraction of %s.' % (root, filename))
+  else:
+    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    tar = tarfile.open(filename)
+    sys.stdout.flush()
+    tar.extractall(data_root)
+    tar.close()
+  data_folders = [
+    os.path.join(root, d) for d in sorted(os.listdir(root))
+    if os.path.isdir(os.path.join(root, d))]
+  if len(data_folders) != num_classes:
+    raise Exception(
+      'Expected %d folders, one per class. Found %d instead.' % (
+        num_classes, len(data_folders)))
+  print(data_folders)
+  return data_folders
 
-# train_filename = '/Users/arpandey/Documents/Arvind Pandey/Research/DeepLearning/notMNIST_large.tar.gz'
-# test_filename = '/Users/arpandey/Documents/Arvind Pandey/Research/DeepLearning/notMNIST_small.tar.gz'
-# train_folders = maybe_extract(train_filename)
-# test_folders = maybe_extract(test_filename)
+train_folders = maybe_extract(train_filename)
+test_folders = maybe_extract(test_filename)
 
+print(train_folders)
+print(test_folders)
 ########################## extracting data finished #################################
 
 
@@ -103,8 +103,7 @@ def load_letter(folder, min_num_images):
   for image in image_files:
     image_file = os.path.join(folder, image)
     try:
-      image_data = (ndimage.imread(image_file).astype(float) -
-                    pixel_depth / 2) / pixel_depth
+      image_data = (ndimage.imread(image_file).astype(float) - pixel_depth / 2) / pixel_depth
       if image_data.shape != (image_size, image_size):
         raise Exception('Unexpected image shape: %s' % str(image_data.shape))
       dataset[num_images, :, :] = image_data
@@ -142,13 +141,11 @@ def maybe_pickle(data_folders, min_num_images_per_class, force=False):
   return dataset_names
 
 
-train_folders = ['notMNIST_large/A', 'notMNIST_large/B', 'notMNIST_large/C', 'notMNIST_large/D', 'notMNIST_large/E', 'notMNIST_large/F', 'notMNIST_large/G', 'notMNIST_large/H', 'notMNIST_large/I', 'notMNIST_large/J']
-test_folders = ['notMNIST_small/A', 'notMNIST_small/B', 'notMNIST_small/C', 'notMNIST_small/D', 'notMNIST_small/E', 'notMNIST_small/F', 'notMNIST_small/G', 'notMNIST_small/H', 'notMNIST_small/I', 'notMNIST_small/J']
-
 train_datasets = maybe_pickle(train_folders, 45000)
 test_datasets = maybe_pickle(test_folders, 1800)
 
-
+print(train_datasets)
+print (test_datasets)
 
 
 def make_arrays(nb_rows, img_size):
